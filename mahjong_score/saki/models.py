83ee7ca61c6dd740ce2397cde84b2f30f2201644
TerauchiAkira <terauchi@ss.cs.osakafu-u.ac.jp>
2019-05-25 15:02:31 +0900

    add models Player. Game

--- mahjong_score/saki/models.py
+++ mahjong_score/saki/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=30)
+    point = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    date = models.DateField()
+    players = models.ManyToManyField(Player, blank=True)
+
+    def __str__(self):
+        lis = [p.name for p in self.players.all()]
+        return "{} : [{}, {}, {}, {}]".format(self.date, lis[0], lis[1], lis[2], lis[3])
+
+
+# class Hand(models.Model):
+#     hand_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+#     parent = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True)  # 親
+#     nth = models.IntegerField(default=0)  # N本場
+#     round_choices = ('東', '南', '西', '北')
+#     round = models.CharField(max_length=1, choices=round_choices)
+#     hora_choices = ('ロン', 'ツモ', '流局')
+#     hora = models.CharField(max_length=2, choices=hora_choices)
+#     discarder = models.ForeignKey(Player, on_delete=models.SET_NULL)
+#     point = models.IntegerField()
