4cba9918dfbe6ab82544a66e19739a0dce82c3af
yoshiki\yoshiki aono <aono@ss.cs.osakafu-u.ac.jp>
2019-06-05 23:42:57 +0900

    add model KyokuPlayer by aono

--- mahjong_score/saki/models.py
+++ mahjong_score/saki/models.py
@@ -32,16 +32,37 @@ class Game(models.Model):
 
 class Kyoku(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    oya = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='oya')  # 親
+    #oya = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='oya')  # 親
     kyoku = models.IntegerField(default=1)  # 局
     honba = models.IntegerField(default=0)  # 本場
     riichi_bou = models.IntegerField(default=0)  # リー棒
     bakaze = models.CharField(max_length=1)
     agari_type = models.CharField(max_length=10)
     ryukyoku_type = models.CharField(max_length=10, null=True)
-    agari_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='agari_player')  # 上がった人
-    houju_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='houju_player')  # 振り込んだ人
-    point = models.IntegerField()
-
+    ## リー棒が出た時などに局プレイヤのテーブルを参照するなら，↓の３つはいらない(?)
+    #agari_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='agari_player')  # 上がった人
+    #houju_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='houju_player')  # 振り込んだ人
+    #point = models.IntegerField()
+    
     def __str__(self):
         return "{} : {}{}局{}本場".format(self.game, self.bakaze, self.kyoku, self.honba)
+
+
+## 名前これでいいの ?
+class KyokuPlayer(models.Model):
+    kyoku = models.ForeignKey(Kyoku, on_delete=models.CASCADE)
+    
+    player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True)
+    # oya = models.BooleanField(default=True) # 自風でわかるか ?
+    jikaze = models.CharField(max_length=1)
+    
+    point_current = models.IntegerField(default=25000)
+    point_change = models.IntegerField(default=0)
+    
+    # stats を求めるときに，このクラスだけを参照し，Kyoku クラスを参照する必要がないようにしたい．
+    agari = models.BooleanField(default=False)
+    riichi = models.BooleanField(default=False)
+    tenpai = models.BooleanField(default=False)
+    houju = models.BooleanField(default=False)
+    ## naki = models.BooleanField(default=False)
+    ## naki_num = modeld.IntegerField(default=0)
