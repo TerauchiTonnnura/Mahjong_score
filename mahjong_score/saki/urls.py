d682da883fcd870b580cbad1664a972af1c2884e
WataruYoshida <yoshida@ss.cs.osakafu-u.ac.jp>
2019-06-13 20:53:58 +0900

    ロン, ツモの点数のプルダウンの作成

--- mahjong_score/saki/urls.py
+++ mahjong_score/saki/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'saki'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('enter_hand', views.enter_hand_result, name='enter_hand_result'),
+    path('enter_kyoku', views.enter_kyoku_result, name='enter_kyoku_result'),
 ]
--- mahjong_score/saki/views.py
+++ mahjong_score/saki/views.py
@@ -2,16 +2,29 @@ from django.shortcuts import render
 from django.http import Http404
 from .models import Player, Game, Kyoku
 
-from .forms import EnterHand
+from .forms import EnterKyoku, RonForm, TsumoForm
 
 
 def index(request):
     return render(request, 'saki/index.html')
 
 
-def enter_hand_result(request):
+def enter_kyoku_result(request):
     #if request.method == 'GET':
         #raise Http404("Question does not exist")
 
-    f = EnterHand()
-    return render(request, 'saki/enter_hand_result.html', {'form': f})
+    f_enter_kyoku = EnterKyoku()
+    f_ron = RonForm()
+    f_tsumo = TsumoForm()
+
+    # game = Game.objects.get(game_id=4)
+    # tontya = game.ton
+    return render(
+        request,
+        'saki/enter_kyoku_result.html',
+        {
+         'form_EnterKyoku': f_enter_kyoku,
+         'form_Ron': f_ron,
+         'form_Tsumo': f_tsumo
+         }
+    )
