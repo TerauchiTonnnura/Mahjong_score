5ced634b86404712f9571412a0b5d13ba3dc5d62
rikuiikura <iikura@ss.cs.osakafu-u.ac.jp>
2019-06-19 22:47:17 +0900

    def start_game の追加

--- mahjong_score/saki/views.py
+++ mahjong_score/saki/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from saki.forms import StartGame
 from .models import Player, Game, Kyoku
 
 from .forms import EnterHand
@@ -7,6 +8,11 @@ from .forms import EnterHand
 
 def index(request):
     return render(request, 'saki/index.html')
+
+
+def start_game(request):
+    f = StartGame()
+    return render(request, 'saki/start_game.html', {'form': f})
 
 
 def enter_hand_result(request):
