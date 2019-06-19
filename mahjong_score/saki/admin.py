2094fc1644b92668b49c7295744343a41dec6e35
yoshikiaono <bluefield12316330@gmail.com>
2019-06-19 18:49:12 +0900

    Update : models
    Kyoku と KyokuPlayer をいじった．

--- mahjong_score/saki/admin.py
+++ mahjong_score/saki/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Player, Game, Kyoku
+from .models import Player, Game, Kyoku, KyokuPlayer
 
 admin.site.register(Player)
 admin.site.register(Game)
 admin.site.register(Kyoku)
+admin.site.register(KyokuPlayer)
--- mahjong_score/saki/models.py
+++ mahjong_score/saki/models.py
@@ -32,41 +32,39 @@ class Game(models.Model):
 
 class Kyoku(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    #oya = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='oya')  # 親
     kyoku = models.IntegerField(default=1)  # 局
     honba = models.IntegerField(default=0)  # 本場
     riichi_bou = models.IntegerField(default=0)  # リー棒
-    bakaze = models.CharField(max_length=1)
     agari_type = models.CharField(max_length=10)
     ryukyoku_type = models.CharField(max_length=10, null=True)
-    ## リー棒が出た時などに局プレイヤのテーブルを参照するなら，↓の３つはいらない(?)
-    #agari_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='agari_player')  # 上がった人
-    #houju_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='houju_player')  # 振り込んだ人
-    #point = models.IntegerField()
     
     def __str__(self):
         return "{} : {}{}局{}本場".format(self.game, self.bakaze, self.kyoku, self.honba)
+    
+    def _get_bakaze(self):
+        if self.kyoku <= 4:
+            return '東'
+        elif self.kyoku <= 8:
+            return '南'
+    
+    @property
+    def bakaze(self):
+        return self._get_bakaze()
 
 
-## 名前これでいいの ?
 class KyokuPlayer(models.Model):
     kyoku = models.ForeignKey(Kyoku, on_delete=models.CASCADE)
     
     player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True)
-    # oya = models.BooleanField(default=True) # 自風でわかるか ?
     jikaze = models.CharField(max_length=1)
     
-    # point_current = models.IntegerField(default=25000)
     point_change = models.IntegerField(default=0)
     
-    # stats を求めるときに，このクラスだけを参照し，Kyoku クラスを参照する必要がないようにしたい．
     agari = models.BooleanField(default=False)
     riichi = models.BooleanField(default=False)
     tenpai = models.BooleanField(default=False)
     houju = models.BooleanField(default=False)
-    ## naki = models.BooleanField(default=False)
-    ## naki_num = modeld.IntegerField(default=0)
+    chonbo = models.BooleanField(default=False)
     
     def __str__(self):
         return "Kyoku={} : player={}".format(self.kyoku, self.player)
-    
