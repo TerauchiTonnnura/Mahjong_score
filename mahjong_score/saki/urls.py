26a9e661f324a6115aebad92034fc7bbaae62292
rikuiikura <iikura@ss.cs.osakafu-u.ac.jp>
2019-06-19 22:46:38 +0900

    start_game の path を追加

--- mahjong_score/saki/urls.py
+++ mahjong_score/saki/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'saki'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('start_game', views.start_game, name='start_game'),
     path('enter_hand', views.enter_hand_result, name='enter_hand_result'),
 ]
